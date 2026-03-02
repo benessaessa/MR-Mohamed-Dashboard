63d979e7592c8d17eb3c976a25a0cec5f207369a
benessaessa <moh.essa2020@gmail.com>
2026-03-02 22:38:13 +0200

    base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/MR-Mohamed-Dashboard/',
+  base: './',
   plugins: [react()],
   esbuild: {
     loader: 'jsx',
